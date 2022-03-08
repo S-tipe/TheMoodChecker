90784e00d8f20682c8b38f4dc84f7dc22b733493
Stipe Roguljić <stipe.roguljic@gmail.com>
2022-03-08 19:41:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+ime = input("upiši svoje ime: ")
+mood = input("kako se osjećaš? ")
+if mood == "sretno":
+    print("Bok, " + ime + " super je da si sretan")
+elif mood == "nervozno":
+    print("Bok, " + ime + " udahni duboko 3 puta!")
+elif mood == "tužno":
+    print("Bok, " + ime + " razvedri se!")
+else:
+    print("Sve će biti ok!")
